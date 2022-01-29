d03d08dfa3e66bd96cf461b8bed8507656eba528
Hampton <94856875+Hampton-Black@users.noreply.github.com>
2022-01-28 23:52:12 -0500

    Add live voting functionality, and update tests.

--- src/js/app.js
+++ src/js/app.js
@@ -27,7 +27,34 @@ App = {
       // connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
 
+      App.listenForEvents();
       return App.render();
+    });
+  },
+
+  listenForEvents: function() {
+    App.contracts.Election.deployed().then(function(instance) {
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        // reload when a new vote is recorded
+        App.render();
+      });
+    });
+  },
+
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
     });
   },
 
@@ -55,6 +82,9 @@ App = {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
       for(var i=1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
@@ -64,7 +94,17 @@ App = {
           //render candidate Result
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
           candidatesResults.append(candidateTemplate);
+
+          // render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>";
+          candidatesSelect.append(candidateOption);
         });
+      }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
       }
 
       loader.hide();
